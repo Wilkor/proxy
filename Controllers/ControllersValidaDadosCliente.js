cc753d62ade55908f14b422867302283ca46f8a7
wilkor <wilkorguitarrista@msn.com>
2020-06-01 22:13:50 -0300

    ajuste em valida dados do cliente

--- Controllers/ControllersValidaDadosCliente.js
+++ Controllers/ControllersValidaDadosCliente.js
@@ -3,27 +3,30 @@
 
   validaDadosCliente =  (req, res) => {
 
-  const payload = req.body
+  const payload = req.body;
+
+  console.log('payload', payload)
   const headers = {
     headers: {
       'Content-Type': 'application/json',
       'Authorization': req.headers['authorization']
     }}
 
-     axios.post(config.urlValidaDadosCliente, payload,headers).then((resp) => {
+  //    axios.post(config.urlValidaDadosCliente, payload,headers).then((resp) => {
 
-        const jsonText3 = JSON.stringify(resp.data);
-        const responseObject3 = JSON.parse(jsonText3);
-        console.log(resp.response);
-        res.send('oi');
-        //res.status(resp.response.status).send(responseObject3);
+  //       const jsonText3 = JSON.stringify(resp.data);
+  //       const responseObject3 = JSON.parse(jsonText3);
+  //       console.log(resp.data);
+  //       res.send('oi');
+  //       //res.status(resp.response.status).send(responseObject3);
 
-    }).catch((err) => {
+  //   }).catch((err) => {
 
-       console.log(err.response);
-       res.send('error');
-      //res.status(err.response.status).json({error: err.response.statusText})
-   });
+  //      console.log(err.response);
+  //      res.send('error');
+  //     //res.status(err.response.status).json({error: err.response.statusText})
+  //  });
+  res.send('teste');
 
 }
 
