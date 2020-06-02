4da41d31714017ed4dba10cff5b7fde7eaeb686c
wilkor <wilkorguitarrista@msn.com>
2020-06-01 22:19:33 -0300

    ajuste da validação do cliente

--- Controllers/ControllersValidaDadosCliente.js
+++ Controllers/ControllersValidaDadosCliente.js
@@ -12,21 +12,20 @@
       'Authorization': req.headers['authorization']
     }}
 
-  //    axios.post(config.urlValidaDadosCliente, payload,headers).then((resp) => {
+     axios.post(config.urlValidaDadosCliente, payload,headers).then((resp) => {
 
-  //       const jsonText3 = JSON.stringify(resp.data);
-  //       const responseObject3 = JSON.parse(jsonText3);
-  //       console.log(resp.data);
-  //       res.send('oi');
-  //       //res.status(resp.response.status).send(responseObject3);
+        const jsonText3 = JSON.stringify(resp.data);
+        const responseObject3 = JSON.parse(jsonText3);
+       
+        res.status(resp.response.status).send(responseObject3);
 
-  //   }).catch((err) => {
+    }).catch((err) => {
 
-  //      console.log(err.response);
-  //      res.send('error');
-  //     //res.status(err.response.status).json({error: err.response.statusText})
-  //  });
-  res.send('teste');
+       console.log(err.response);
+      
+      res.status(err.response.status).json({error: err.response.statusText})
+   });
+  
 
 }
 
