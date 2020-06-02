146c09f8327b869ae02a1771501e921526a576ef
wilkor <wilkorguitarrista@msn.com>
2020-06-02 16:16:41 -0300

    descobrindo path

--- Controllers/ControllerTalking.js
+++ Controllers/ControllerTalking.js
@@ -1,12 +1,13 @@
 const axios = require('axios');
+const path = require('path');
  talking =  async (req,res) => {
 
- const userInfo = req.body.id
- const multThree = userInfo.split('.net')[1];
- const userId = userInfo.split('.net')[0];
+//  const userInfo = req.body.id
+//  const multThree = userInfo.split('.net')[1];
+//  const userId = userInfo.split('.net')[0];
 
 
-   console.log(multThree)
+   console.log('opa', path.resolve('./download'))
    res.send('ok')
  
 
