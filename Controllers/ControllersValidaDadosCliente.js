0aa4d176822363fa2cec7df3343e06d1a1600850
wilkor <wilkorguitarrista@msn.com>
2020-06-02 01:48:29 -0300

    forçando status na validação do cliente

--- Controllers/ControllersValidaDadosCliente.js
+++ Controllers/ControllersValidaDadosCliente.js
@@ -17,15 +17,12 @@
         const jsonText3 = JSON.stringify(resp.data);
         const responseObject3 = JSON.parse(jsonText3);
        
-        res.status(resp.response.status).send(responseObject3);
+        res.status(200).send(responseObject3);
 
     }).catch((err) => {
-      if (err.response.status) {
-
-        res.status(err.response.status).json({error: err.response.statusText});
-      } else {
-        res.status(400).json({error: err.response.statusText});
-      }
+  
+      res.status(400).json({error: err.response.statusText});
+      
    });
   
 
