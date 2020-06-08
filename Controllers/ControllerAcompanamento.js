1e9e9764d74def32721fe9eaf7c7ee84bb260b36
wilkor <wilkorguitarrista@msn.com>
2020-06-08 01:03:29 -0300

    logando payload da api acompanhamento

--- Controllers/ControllerAcompanamento.js
+++ Controllers/ControllerAcompanamento.js
@@ -12,6 +12,9 @@ acompanhamento =  (req, res) => {
 
   const {idProposta, idTpRegistro, descricaoFase, status } = req.body;
 
+
+
+
     const payload = {
           idProposta,
           idTpRegistro,
@@ -19,23 +22,20 @@ acompanhamento =  (req, res) => {
           status
          }
 
-        try {
-          
-          axios.post(config.urlAcompanhamento, payload,headers).then((resp) => {
   
-             const jsonText3 = JSON.stringify(resp.data);
-             const responseObject3 = JSON.parse(jsonText3);
-             res.json(responseObject3);
-             
-           }).catch((err) => {
-            res.status(err.response.status).json({error: err.response.statusText})
-          });
-        } catch (error) {
-
-          console.log(error)
+         console.log(payload);
           
-        }
-
+          // axios.post(config.urlAcompanhamento, payload,headers).then((resp) => {
+  
+          //    const jsonText3 = JSON.stringify(resp.data);
+          //    const responseObject3 = JSON.parse(jsonText3);
+          //    res.json(responseObject3);
+             
+          //  }).catch((err) => {
+          //   res.status(err.response.status).json({error: err.response.statusText})
+          // });
+   
+        res.send('ok')
 
       }
   
