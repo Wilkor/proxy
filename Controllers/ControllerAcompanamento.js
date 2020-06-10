6eb98c65a513b4e4037a10d0124935b6bdc68dd2
wilkor <wilkorguitarrista@msn.com>
2020-06-10 00:02:55 -0300

    printando o request

--- Controllers/ControllerAcompanamento.js
+++ Controllers/ControllerAcompanamento.js
@@ -11,15 +11,15 @@ acompanhamento =  (req, res) => {
       }}
 
 
-    const payload = req.body;
+    const payload = req.body
 
       try {
         
         axios.post(config.urlAcompanhamento, payload,headers).then((resp) => {
   
-           const jsonText3 = JSON.stringify(resp.data);
-           const responseObject3 = JSON.parse(jsonText3);
-           res.json(responseObject3);
+           //const jsonText3 = JSON.stringify(resp.data);
+          // const responseObject3 = JSON.parse(jsonText3);
+           res.json({message:'ok'});
            
          }).catch((err) => {
   
