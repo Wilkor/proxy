b21c107252edf0383b9065bab51b1ed2d17c4d18
wilkor <wilkorguitarrista@msn.com>
2020-06-09 23:38:06 -0300

    printando o request

--- Controllers/ControllerAcompanamento.js
+++ Controllers/ControllerAcompanamento.js
@@ -3,29 +3,31 @@ const config = require('../config/index');
 
 acompanhamento =  (req, res) => {
 
-    const headers = {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': req.headers['authorization']
-      }}
+   console.log('acompanhamento', JSON.stringify(req));
+
+    // const headers = {
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Authorization': req.headers['authorization']
+    //   }}
 
 
-    const payload = req.body;
+    // const payload = req.body;
 
-    console.log('Payload Acompanhamento', payload);
+    // console.log('Payload Acompanhamento', payload);
 
           
-          axios.post(config.urlAcompanhamento, payload,headers).then((resp) => {
+    //       axios.post(config.urlAcompanhamento, payload,headers).then((resp) => {
   
-             const jsonText3 = JSON.stringify(resp.data);
-             const responseObject3 = JSON.parse(jsonText3);
-             res.json(responseObject3);
+    //          const jsonText3 = JSON.stringify(resp.data);
+    //          const responseObject3 = JSON.parse(jsonText3);
+    //          res.json(responseObject3);
              
-           }).catch((err) => {
-            res.status(err.response.status).json({error: err.response.statusText})
-          });
+    //        }).catch((err) => {
+    //         res.status(err.response.status).json({error: err.response.statusText})
+    //       });
    
-       
+      res.send('ok')
       }
   
 module.exports = {acompanhamento}
