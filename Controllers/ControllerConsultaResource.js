c1b2b4d999e93157f1456aa0f8f7f3e01a4e1c6c
wilkor <wilkorguitarrista@msn.com>
2020-06-11 12:49:05 -0300

    liberando get resource

--- Controllers/ControllerConsultaResource.js
+++ Controllers/ControllerConsultaResource.js
@@ -20,7 +20,9 @@ getResource =  async (req, res) => {
     const response = await axios.post(`${config.baseUrl}/commands`, payload,headers);
     const jsonText = JSON.stringify (response.data);
     const responseObject = JSON.parse (jsonText);
-    res.status(200).json(responseObject);
+
+    const pre = `<pre>${responseObject}</pre>`
+    res.status(200).send(pre);
      
  }
 module.exports = {
