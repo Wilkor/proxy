f1c5f46141b6dc1352e8e0788e205a6773fd48e8
wilkor <wilkorguitarrista@msn.com>
2020-06-13 01:42:55 -0300

    apontando para prod

--- config/index.js
+++ config/index.js
@@ -2,12 +2,12 @@ module.exports = {
   baseUrl:'https://http.msging.net',
   uploads:'./download',
   pdf:'./save_file_path',
-  urlAcompanhamento:'https://api.safrafinanceira.com.br/apl-api-formalizacao-consignado/api/v1/AcompanhamentoFormalizacao',
-  urlToken:'https://api.safrafinanceira.com.br/apl-api-formalizacao-consignado/api/v1/Token',
-  urlCancelmentoFormalizacao:'https://api.safrafinanceira.com.br/apl-api-formalizacao-consignado/api/v1/CancelarFormalizacao',
-  urlArtefato:'https://api.safrafinanceira.com.br/apl-api-formalizacao-consignado/api/v1/Artefatos',
-  urlValidaDadosCliente: 'https://api.safrafinanceira.com.br/apl-api-formalizacao-consignado/api/v1/ValidacaoDadosCliente',
-  urlAssinaProposta: 'https://api.safrafinanceira.com.br/apl-api-formalizacao-consignado/api/v1/AssinarProposta',
-  urlReabirFormalizacao: 'https://api.safrafinanceira.com.br/apl-api-formalizacao-consignado/api/v1/ReabrirFormalizacao'
+  urlAcompanhamento:'https://api-h.safrafinanceira.com.br/apl-api-formalizacao-consignado/api/v1/AcompanhamentoFormalizacao',
+  urlToken:'https://api-h.safrafinanceira.com.br/apl-api-formalizacao-consignado/api/v1/Token',
+  urlCancelmentoFormalizacao:'https://api-h.safrafinanceira.com.br/apl-api-formalizacao-consignado/api/v1/CancelarFormalizacao',
+  urlArtefato:'https://api-h.safrafinanceira.com.br/apl-api-formalizacao-consignado/api/v1/Artefatos',
+  urlValidaDadosCliente: 'https://api-h.safrafinanceira.com.br/apl-api-formalizacao-consignado/api/v1/ValidacaoDadosCliente',
+  urlAssinaProposta: 'https://api-h.safrafinanceira.com.br/apl-api-formalizacao-consignado/api/v1/AssinarProposta',
+  urlReabirFormalizacao: 'https://api-h.safrafinanceira.com.br/apl-api-formalizacao-consignado/api/v1/ReabrirFormalizacao'
 
 }
