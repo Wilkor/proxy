e857432f0d4df87d3d45a0fee4eb47e0107105a7
wilkor <wilkorguitarrista@msn.com>
2020-06-25 19:06:08 -0300

    subindo proxy para contratação

--- Controllers/Token/ControllerToken.js
+++ Controllers/Token/ControllerToken.js
@@ -4,21 +4,15 @@ const config = require('../../config/index');
 token =  (req, res) => {
 
   
-  const headers = {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': req.headers['authorization']
-    }}
-  
-    axios.post(`${config.contratacao.baseUrl}/api/v1/Token`, req.body, headers).then((resp) => {
+    axios.post(`${config.contratacao.baseUrl}/api/v1/Token`, req.body).then((resp) => {
       
     const jsonText = JSON.stringify(resp.data);
     const responseObject = JSON.parse(jsonText);
     res.json(responseObject);
 
   }).catch((err) => {
-  
-   res.status(err.response.status).json({error: err.response.statusText})
+   console.log(err)
+   //res.status(err.response.status).json({error: err.response.statusText})
  });
 
 }
