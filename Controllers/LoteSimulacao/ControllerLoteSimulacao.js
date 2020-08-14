0071091086554538e4108b0720c88f88a93569bb
Giovanni Vilella <gioatt@br.ibm.com>
2020-08-14 17:02:46 -0300

    lote simula

--- Controllers/LoteSimulacao/ControllerLoteSimulacao.js
+++ Controllers/LoteSimulacao/ControllerLoteSimulacao.js
@@ -11,7 +11,7 @@ loteSimulacao =  (req, res) => {
 
 
   
-    axios.post(`${config.contratacao.baseUrl}/api/v1/LoteSimulacao/${req.params.telefone}`, req.body, headers).then((resp) => {
+    axios.get(`${config.contratacao.baseUrl}/api/v1/LoteSimulacao/${req.params.telefone}`, headers).then((resp) => {
       
     const jsonText = JSON.stringify(resp.data);
     const responseObject = JSON.parse(jsonText);
