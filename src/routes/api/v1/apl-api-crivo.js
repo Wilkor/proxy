ff537005dd2612aed31f77726fbff97cc9d6f8ee
Giovanni Vilella <gioatt@br.ibm.com>
2020-09-01 15:54:19 -0300

    crivo routes

--- src/routes/api/v1/apl-api-crivo.js
+++ src/routes/api/v1/apl-api-crivo.js
@@ -3,10 +3,8 @@ const routes = express.Router();
 
 const ControllerTokenCrivo = require('../../../ControllersCrivo/Token/ControllerTokenCrivo');
 const ControllerCrivo = require('../../../ControllersCrivo/CrivoContratacaoWhatsApp/ControllerCrivo');
-const ControllerCrivoReduced = require('../../../ControllersCrivo/CrivoContratacaoWhatsApp/ControllerCrivoReduced');
 
-routes.post('/CrivoToken', ControllerTokenCrivo.token);
+routes.post('/Token', ControllerTokenCrivo.token);
 routes.post('/CrivoContratacaoWhatsApp', ControllerCrivo.crivo);
-routes.post('/CrivoContratacaoWhatsAppReduced', ControllerCrivoReduced.crivo);
 
 module.exports = routes;
